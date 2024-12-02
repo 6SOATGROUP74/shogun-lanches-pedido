6c4a6adfeb2d9a963d57d73f16b22a20946508f4
igusvs <igoralves159@gmail.com>
2024-12-02 03:53:09 -0300

    Produto mapper

--- src/main/java/com/example/demo/infrastructure/integration/shogun/produto/ProdutoMapper.java
+++ src/main/java/com/example/demo/infrastructure/integration/shogun/produto/ProdutoMapper.java
@@ -1,6 +1,5 @@
 package com.example.demo.infrastructure.integration.shogun.produto;
 
-import com.example.demo.adapter.controller.request.produto.ProdutoRequest;
 import com.example.demo.core.domain.Produto;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -10,6 +9,5 @@ public interface ProdutoMapper {
 
     ProdutoMapper INSTANCE = Mappers.getMapper(ProdutoMapper.class);
 
-    Produto mapFrom(ProdutoRequest produtoRequest);
     Produto mapFrom(ProdutoResponse produtoResponse);
 }
