caa90867595f050cf44f20299d5771da72e22d81
im4i <rodrigoimai@hotmail.com>
2024-12-03 05:11:30 -0300

    Implementa pipeline

--- src/test/java/com/example/demo/bdd/DefinicaoPassos.java
+++ src/test/java/com/example/demo/bdd/DefinicaoPassos.java
@@ -30,25 +30,4 @@ public class DefinicaoPassos {
     public void retorno_da_chamada_de_pedidos() {
         response.then().statusCode(HttpStatus.OK.value());
     }
-
-    @Quando("for solicitada a criação de um pedido")
-    public void for_solicitada_a_criação_de_um_pedido() {
-        ComposicaoRequest composicaoRequest = new ComposicaoRequest();
-        composicaoRequest.setIdProduto(15L);
-        composicaoRequest.setQuantidade(5);
-        PedidoRequest pedidoRequest = new PedidoRequest();
-        pedidoRequest.setIdCliente("f090626a-6599-4c6e-b80d-d3a88872dfdd");
-        pedidoRequest.setComposicao(Arrays.asList(composicaoRequest));
-
-        response = given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(pedidoRequest)
-                .when()
-                .post(ENDPOINT_PEDIDOS);
-    }
-
-    @Então("deverá ser criado com sucesso")
-    public void deverá_ser_criado_com_sucesso() {
-        response.then().statusCode(HttpStatus.CREATED.value());
-    }
 }
